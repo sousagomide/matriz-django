2ff498fc28406ada5e36ef733265bf215e90f21e
sousagomide <sousagomide@gmail.com>
2023-12-18 10:21:41 -0300

    2023-12-18-1021-update

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from home.views import IndexView
+
+from home.views import IndexView, SobreView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre')
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 
+
 class IndexView(TemplateView):
 
     template_name = 'index.html'
+
+
+class SobreView(TemplateView):
+
+    template_name = 'sobre.html'
